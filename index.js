4ca18e1faa044ea0a8f75bdd6495d96f43601ca8
Vinicius tamae <vinicius.tamae@pedbot.com.br>
2024-08-27 19:50:50 -0300

    Card do fone criado e modifição na navbar

---
+++ index.js
@@ -0,0 +1,19 @@
+const countdownDate = new Date("Aug 28, 2024 23:59:59").getTime();
+
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000);
