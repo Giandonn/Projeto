d579498791f13ac32583bdb56a05acb61e4c054d
AyrtonlinOn <ayrtonlimaol22@gmail.com>
2024-08-28 00:12:18 -0300

    Troca de headphone

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const pinkImg = document.querySelector('.headphone-img.pink');
+    const redImg = document.querySelector('.headphone-img.red');
+    const colorBtns = document.querySelectorAll('.color-btn');
+
+    colorBtns.forEach(button => {
+        button.addEventListener('click', () => {
+            const selectedColor = button.getAttribute('data-img');
+
+            if (selectedColor === 'img/red.png') {
+                pinkImg.style.transform = 'translateY(0)';
+                redImg.style.transform = 'translateY(100%)';
+            } else {
+                pinkImg.style.transform = 'translateY(-100%)';
+                redImg.style.transform = 'translateY(0)';
+            }
+
+            colorBtns.forEach(btn => btn.classList.remove('selected'));
+            button.classList.add('selected');
+        });
+    });
+});
